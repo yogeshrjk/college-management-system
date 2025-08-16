6057a84ea0c4fcd25f52baab751fb2f532d2bd72
Yogesh <yrajak9@gmail.com>
2025-08-16 11:21:54 +0530

    server keep awake

--- server/index.js
+++ server/index.js
@@ -46,6 +46,10 @@ const app = express();
       })
     );
 
+    app.get("/health", (req, res) => {
+      res.status(200).send("ok");
+    });
+
     // Connect to MongoDB
     await connectMongoDb(process.env.MONGO_URI);
     //Start Express Server
